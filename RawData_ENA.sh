fd2aa0ed6a341bcdc348283970e7c695a29549cf
Sarita Maurya <115336007+Saritamaurya1234@users.noreply.github.com>
2024-07-01 17:27:39 +0530

    Create RawData_ENA.sh

---
+++ RawData_ENA.sh
@@ -0,0 +1,10 @@
+###### Raw reads download from ENA
+Project: PRJNA548947
+Secondary Study Accession:SRP201481
+Study Title:Single cell RNA-sequencing of normal and fibrotic lungs in mouse and human
+Center Name:Medicine, UCSF
+Study Name:Single cell RNA-sequencing of normal and fibrotic lungs in mouse and human
+
+###### open terminal
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR930/008/SRR9304738/SRR9304738.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR930/000/SRR9304740/SRR9304740.fastq.gz
